5b88727adfabd87dde86e1cba226aac09407034a
Noel Ortiz <noel@Noels-MacBook-Pro-2.local>
2025-03-23 13:08:40 -0700

    finished form submission behavior

--- main.js
+++ main.js
@@ -3,14 +3,29 @@ const popupBtn = document.querySelector('.popup-btn');
 const successMsg = document.querySelector('.popup-msg-wrapper');
 const wrapper = document.querySelector('.wrapper');
 const emailInput = document.querySelector('#email');
+const errorLabelMsg = document.querySelector('.error-label-msg');
 
 
-
+errorLabelMsg.classList.toggle('h');
 function handleSubmit(e){
 	e.preventDefault();
-	wrapper.classList.add('h');
-	successMsg.classList.toggle('h');
-	console.log('form submitted');
+	if (emailInput.value.trim() === ''){
+		emailInput.classList.add('error-msg');
+		emailInput.focus();
+		errorLabelMsg.classList.toggle('h');
+		console.log(emailInput);
+		console.log(errorLabelMsg);
+		return;
+	} else{
+		if (!errorLabelMsg.classList.contains('h')){
+			errorLabelMsg.classList.toggle('h');
+			emailInput.classList.remove('error-msg');
+
+		}
+		wrapper.classList.add('h');
+		successMsg.classList.toggle('h');
+	}
+
 }
 
 function handleClick(e){
